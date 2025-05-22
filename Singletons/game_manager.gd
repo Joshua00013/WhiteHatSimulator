54c309efffa4eacb34f0bf91cdf323400144c870
Joshua <149653102+Joshua00013@users.noreply.github.com>
2025-05-22 16:14:09 +0800

    Polished character movement

---
+++ Singletons/game_manager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var ui_active : bool = false #Variable for checking if theres an active ui. Defaulted to false as the player doesnt spawn with UI on
+var game_paused : bool = false # Variable for pausing the game
--- node_3d.gd
+++ node_3d.gd
@@ -1,40 +1,51 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-var sensitivity := 5
-@onready var camera_3d = $Camera3D
+const JUMP_VELOCITY = 10
+@export var sensitivity := 0.25
+@export var min_angle = -80
+@export var max_angle = 90
+@export var accel = 16
+@export var look_rotation : Vector2
+@onready var camera_3d = $Head/Camera3D
+@onready var head = $Head
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		rotation_degrees.y -= event.relative.x * 0.2
-		camera_3d.rotation_degrees.x -= event.relative.y * 0.2
-		camera_3d.rotation_degrees.x = clamp(
-			camera_3d.rotation_degrees.x, -60, 60.0
-		)
-	elif event.is_action_pressed("ui_cancel"):
+		look_rotation.y -= (event.relative.x * sensitivity) #Subtract horizontal mouse movement (x) from the head's y rotation (horizontal rotation from y pole)
+		look_rotation.x -= (event.relative.y * sensitivity)
+		look_rotation.x = clamp(look_rotation.x, min_angle, max_angle) #Minimum and maximum rotation of the vertical rotation
+		
+	elif event.is_action_pressed("interact") && GameManager.ui_active == true || event.is_action_pressed("pause"):
+		#Set the cursor visibility to default when a UI is active or game has been paused.
+		#Maybe handle game pause/resume and ui active in gamemanager later instead of in the player node.
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if direction: #Used lerping for smoother movement
+		velocity.x = lerp(velocity.x, direction.x * SPEED, accel * delta)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, accel * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, 0.0, accel * delta)
+		velocity.z = lerp(velocity.z, 0.0, accel * delta)
 
 	move_and_slide()
+	
+	head.rotation_degrees.x = look_rotation.x # Set the vertical rotation to the head
+	rotation_degrees.y = look_rotation.y # Set the horizontal rotation to the whole body
+	
