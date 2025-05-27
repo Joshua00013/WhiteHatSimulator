b5d53aef58ae58b6ebb0cd4f6b9a1020035b9825
Joshua <149653102+Joshua00013@users.noreply.github.com>
2025-05-27 18:31:48 +0800

    Emulate cursor setting

--- Singletons/game_manager.gd
+++ Singletons/game_manager.gd
@@ -1,8 +1,17 @@
 extends Node
 
+@onready var emulate_mouse = ProjectSettings.get_setting("input_devices/pointing/emulate_mouse_from_touch")
+
 var ui_active : bool = false #Variable for checking if theres an active ui. Defaulted to false as the player doesnt spawn with UI on
 var game_paused : bool = false # Variable for pausing the game
 var is_crouching :bool = false
 
 var player_camera : Camera3D
 var player : CharacterBody3D
+
+func _ready():
+	match OS.get_name():
+		"Windows":
+			ProjectSettings.set_setting("input_devices/pointing/emulate_mouse_from_touch", false)
+		"Android":
+			ProjectSettings.set_setting("input_devices/pointing/emulate_mouse_from_touch", true)
