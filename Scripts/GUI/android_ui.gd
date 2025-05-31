59308c026610c294baf08cad376631c83cad3417
Joshua <149653102+Joshua00013@users.noreply.github.com>
2025-06-01 00:46:31 +0800

    Mobile overlay

--- Scripts/GUI/android_ui.gd
+++ Scripts/GUI/android_ui.gd
@@ -2,7 +2,13 @@ extends CanvasLayer
 
 func _ready():
 	match OS.get_name():
-		"Windows":
-			visible = false
+		#"Windows":
+			#visible = false
 		"Android":
 			visible = true
+			
+func _input(event):
+	if event.is_action("inventory") && GameManager.ui_active == true:
+		visible = false
+	elif event.is_action("inventory") && GameManager.ui_active == false:
+		visible = true
