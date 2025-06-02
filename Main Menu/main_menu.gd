e474dd57b55e6580b4ff33cb4fd3abf2178abc52
macky-candelario <candelariomacky433@gmail.com>
2025-06-02 22:11:48 +0800

    Additional Clipboard Scene

--- Main Menu/main_menu.gd
+++ Main Menu/main_menu.gd
@@ -10,7 +10,7 @@ func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_settings_pressed():
-	pass 
+	get_tree().change_scene_to_file("res://Main Menu/mainMenu#2/Main_menu/settings.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
