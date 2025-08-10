621e656ea2f199b1d3d6102b1860f4ac1001378f
Joshua <149653102+Joshua00013@users.noreply.github.com>
2025-08-11 00:11:03 +0800

    non working door

--- Scripts/Interactables/door.gd
+++ Scripts/Interactables/door.gd
@@ -1,11 +1,18 @@
 extends Node3D
 
+@onready var animation_player = $AnimationPlayer
+
 var is_open := false
 
 func _on_interactable_interact_triggered():
+	if animation_player.is_playing():
+		return
+	
 	if is_open == false:
-		rotate_y(90)
+		print("Opened")
 		is_open = true
+		animation_player.play("open")
 	else:
-		rotate_y(-90)
-		is_open = false
+		print("Closed")
+		#is_open = false
+		#animation_player.play("close")
