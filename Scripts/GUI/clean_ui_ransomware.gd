33619b1f177afef163beef9255885bc7ad20ecde
Joshua Madrilejos <joshuamadrilejos@gmail.com>
2025-08-15 11:52:19 +0800

    Added TODO plugin

--- Scripts/GUI/clean_ui_ransomware.gd
+++ Scripts/GUI/clean_ui_ransomware.gd
@@ -6,6 +6,7 @@ extends MarginContainer
 var step: int = 0
 
 func _on_back_button_down() -> void:
+	# TODO: Set limits on back and next
 	if step > 1:
 		step -= 1
 		set_step(step)
@@ -18,6 +19,6 @@ func set_step(step: int) -> void:
 	match step:
 		1:
 			code_edit.text = "This is step 1"
-			#Adjust overlay.material dimensions accordingly here
+			# TODO: Adjust overlay.material dimensions accordingly here
 		2:
 			code_edit.text = "This is step 1\nThis is step 2"
