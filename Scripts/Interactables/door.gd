9e1d57f0e80310b0bb54a57181d5aeda4ee42a18
Joshua Madrilejos <joshuamadrilejos@gmail.com>
2025-09-02 18:07:03 +0800

    IconTextures

--- Scripts/Interactables/door.gd
+++ Scripts/Interactables/door.gd
@@ -1,10 +1,22 @@
 extends Node3D
 
 @onready var animation_player = $AnimationPlayer
+@onready var interactable: Interactable = $Interactable
 
 var is_open := false
 
+@export var security_door := false
+@export var locked : bool = false
+
+func _ready() -> void:
+	if locked:
+		interactable.prompt_message = "Locked"
+
 func _on_interactable_interact_triggered():
+	if locked:
+		#TODO: Add a message saying door is locked
+		return
+	
 	if animation_player.is_playing():
 		return
 	
