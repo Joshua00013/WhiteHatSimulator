b1d58132f2b1fcb1f0deeee31a2e1a0e915e37ac
Joshua <149653102+Joshua00013@users.noreply.github.com>
2025-09-04 12:49:33 +0800

    Refactoring and adjustment of computer.tscn

--- GUI/MainMenuUI/main_menu.gd
+++ GUI/MainMenuUI/main_menu.gd
@@ -1,16 +1,12 @@
 extends Control
 
-@export var animation:AnimationPlayer
-
-func _ready():
-	animation.play("main_menu_start")
+@onready var settings = $Settings
 
 func _on_start_pressed():
-	animation.play("main_menu_clicked_start_new_game")
-	#get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://Main Menu/mainMenu#2/Main_menu/settings.tscn")
+	settings.visible = true
 
 func _on_exit_pressed():
 	get_tree().quit()
@@ -18,13 +14,3 @@ func _on_exit_pressed():
 
 func _on_button_4_button_down():
 	get_tree().change_scene_to_file("res://Scenes/gui/load_menu.tscn")
-
-
-func _on_fileless_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Stages/Fileless/stage_fileless.tscn")
-
-func _on_phishing_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Stages/Phishing/stage_phishing.tscn")
-
-func _on_dos_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Stages/DOS/stage_dos.tscn")
