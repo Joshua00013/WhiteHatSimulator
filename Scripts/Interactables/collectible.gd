5270ce3ac9dd85151e36eda6fdab2219aa25c1c1
Joshua Madrilejos <149653102+Joshua00013@users.noreply.github.com>
2025-09-09 19:15:30 +0800

    Revert "Cleanup of Malwares & Flashdrive Script"

--- Scripts/Interactables/collectible.gd
+++ Scripts/Interactables/collectible.gd
@@ -1,4 +1,4 @@
-extends CharacterBody3D
+extends Node3D
 
 @export var resource : InvItem
 
--- Singletons/game_manager.gd
+++ Singletons/game_manager.gd
@@ -37,6 +37,7 @@ func remove_item(item:InvItem):
 func check_inv(item:InvItem):
 	return inv.check_inv(item)
 
+
 func adjust_screen_size():
 	var screen_size = DisplayServer.screen_get_size()
 	var win = get_window()
