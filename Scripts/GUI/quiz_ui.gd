c5f13bb8a664d57f0e54672d4d04208df1600cb8
macky-candelario <candelariomacky433@gmail.com>
2025-09-12 17:54:36 +0800

    additional questioners for ransomware

--- Scripts/GUI/quiz_ui.gd
+++ Scripts/GUI/quiz_ui.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 @export var score_label : Label
 @export var status : Label
-@export var passing_score : int = 5
+@export var passing_score : int = 10
 @onready var finished_window: Control = $FinishedWindow
 
 func _on_quiz_card_quiz_finished(point_counter: int) -> void:
