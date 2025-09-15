b3268f78c718905566fbb074e072d73f1a13bbf8
Joshua <149653102+Joshua00013@users.noreply.github.com>
2025-09-15 17:46:13 +0800

    ransomware ui fixes

--- Scenes/gui/Ransomware/clean_ransomware.gd
+++ Scenes/gui/Ransomware/clean_ransomware.gd
@@ -8,13 +8,14 @@ signal listener
 
 const TABS := {
 	"Ransomware": 1,
-	"RansomwareEncrypt": 2,
+	"PostRansomwareEncrypt": 2,
 	"RansomwareDecrypt" : 3,
 	"Bruteforce": 4,
 	"Phishing": 7,
 	"Fileless": 5,
 	"DOS": 9,
 	"Login" : 11
+	# TODO : Add all page names here to avoid magic numbers and use the dictionary instead
 }
 
 func _ready() -> void:
@@ -38,17 +39,17 @@ func _on_desktop_default_cyberattack_selected(cyberattack: Variant) -> void:
 			current_tab = TABS.DOS
 	UiManager.show_nav_buttons.emit()
 
-func _on_window_window_exited() -> void:
-	current_tab = 2
-
-func _on_post_ransomware_encrypt_post_ransomware_encrypt_exited() -> void:
-	current_tab = 3
-
 func _on_weaponization_finished() -> void:
 	UiManager.popup.display_popup("Good Job!", "Weaponization is now finished. Now deploy your cyberattack", false)
 	CyberattackManager.weaponization_finished = true
 	desktop_default.hide_cards()
 	current_tab = 0
+
+func _on_window_window_exited() -> void:
+	current_tab = TABS.PostRansomwareEncrypt
+
+func _on_post_ransomware_encrypt_post_ransomware_encrypt_exited() -> void:
+	current_tab = TABS.RansomwareDecrypt
 
 func _on_fileless_scene_change() -> void:
 	current_tab = 6
