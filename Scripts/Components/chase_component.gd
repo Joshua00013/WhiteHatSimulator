c3a698fc7d211abaded9a0a3f23ac0100c982d09
Joshua <149653102+Joshua00013@users.noreply.github.com>
2025-09-18 21:57:24 +0800

    collapsible task box

--- Scripts/Components/chase_component.gd
+++ Scripts/Components/chase_component.gd
@@ -11,5 +11,9 @@ func disable_chase():
 func enable_chase():
 	collision_shape_3d.disabled = false
 
-func _on_body_entered(body):
-	UiManager.game_over_ui.play()
+func _on_body_entered(_body):
+	if Dialogic.current_timeline == null:
+		Dialogic.start("caught")
+		await Dialogic.timeline_ended
+		UiManager.game_over_ui.play()
+		
