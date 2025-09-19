0c3e38e2fa1f8c971d25a10a01b26f5ef7d2a7b0
Joshua <149653102+Joshua00013@users.noreply.github.com>
2025-09-20 00:35:30 +0800

    Bugfix on laptop GUI

--- Scripts/Globals/signal_bus.gd
+++ Scripts/Globals/signal_bus.gd
@@ -26,6 +26,9 @@ signal ransomware_email_sent (email : String)
 signal fileless_email_sent (email : String)
 signal phishing_email_sent (email : String)
 
+signal ransomware_uploaded # Only used for piracy
+signal fileless_uploaded
+
 @warning_ignore("unused_signal")
 signal fileless_terminal_to_desktop
 @warning_ignore("unused_signal")
